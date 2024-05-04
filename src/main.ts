b38e54528e12278b3d00fdc5adbb96cbd130c9a7
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-04 12:17:44 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+// import './style.css';
+
+// // src/main.ts
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// console.log(add(1, 2));
+
+// type User = {
+//   name: string;
+//   age: number;
+// };
+
+// let user: User = {
+//   name: 'Tom',
+//   age: 10,
+// };
+
+// let userJack: User = {
+//   name: 'Jack',
+//   age: 25,
+// };
+
+// export { user, userJack };
+
+// let arrString: string[];
+
+// arrString = ['Text', 1];
+
+// export {};
+
+// let arrNumber: number[];
+
+// arrNumber = [1, 'Text'];
+
+// export {};
